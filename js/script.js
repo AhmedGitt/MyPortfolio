f39a2375e0d09048e87774567a99c0a56adc7a21
Ahmede <s2201198@bc.edu.fi>
2023-03-28 10:46:59 +0300

    Finished the website

--- js/script.js
+++ js/script.js
@@ -32,3 +32,24 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+
+ScrollReveal({ 
+    /*reset: true,*/
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Angular Developer', 'Backend Developer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
